9a6c3b1e45e8fb1ecbbdcb435104c24b348df2e9
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-08 15:23:08 +0200

    Create camera :obj using PerspectiveCamera class

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { Scene } from "three";
+import { Scene, PerspectiveCamera } from "three";
+
+// canvas-container node
+
+let container = document.getElementById("scene-container");
 
 // Create Scene
 const scene = new Scene();
+
+// Perspective Camera Config
+const fov = 35; // Field of View
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1; // the near clipping plane
+const far = 100; // the far clipping plane
+
+// Create Camera
+const camera = new PerspectiveCamera(fov, aspect, near, far);
